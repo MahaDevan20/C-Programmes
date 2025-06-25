41b80b37a75499b773c780e1743cea25fc24171b
Mahadevan Nair M P <mahadevannair16@gmail.com>
2025-06-25 21:37:53 +0530

    Add files via upload

--- MatrixMul.cpp
+++ MatrixMul.cpp
@@ -1,44 +1,54 @@
 #include <stdio.h>
 int main()
 {
-	int a[3][3],b[3][3],c[3][3],r,co,i,j,result=0;
+	int a[3][3],b[3][3],c[3][3],ar,ac,br,bc,i,j,sum=0;
 		printf("Enter the number of column and rows:\n");
-		scanf("%d%d",&r,&co);
+		scanf("%d%d",&ar,&ac);
 		
 		printf("Enter the elements of the first matrix:\n");
-		for(i=0;i<r;i++)
+		for(i=0;i<ar;i++)
 		{
-			for(j=0;j<co;j++)
+			for(j=0;j<ac;j++)
 			{
 				scanf("%d",&a[i][j]);
 			}
 		}
+		
+		printf("Enter the number of column and rows:\n");
+		scanf("%d%d",&br,&bc);
+		if(ac != br){
+			printf("Multiplication is not possible!");
+			return 0;
+		}
 		printf("Enter the elements of the second matrix:\n");
-		for(i=0;i<r;i++)
+		for(i=0;i<br;i++)
 		{
-			for(j=0;j<co;j++)
+			for(j=0;j<bc;j++)
 			{
 				scanf("%d",&b[i][j]);
 			}
 		}
 		
-		for(i=0;i<r;i++)
-		{
-			c[i][j]=0;
-			for(j=0;j<co;j++)
-			{
-				result= a[i][j] * b[j][i];
-				c[i][j] += result;
+			for(i=0;i<ar;i++){
+				for(j=0;j<bc;j++){
+					for(int k=0;k<ac;k++){
+						sum +=a[i][k] * b[k][j];
+					}
+					c[i][j]=sum;
+					sum=0;
+				}
 			}
-		}
+	
+		
 		printf("the product is:\n");
 	
-		for(i=0;i<r;i++)
+		for(i=0;i<ar;i++)
 		{
-			for(j=0;j<co;j++)
+			for(j=0;j<bc;j++)
 			{
-				printf("%d  ",c[j][i]);
+				printf("%d  ",c[i][j]);
 			}
 			printf("\n");
 		}
+		return 0;
 }
